20fe59e836603d31e100c45189e5b8063d29a787
Quyana Barrow <quyanabarrow@Quyanas-MBP.lan>
2023-07-11 14:37:29 -0400

    welcome page UI

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -51,11 +51,13 @@ const Welcome = () => {
                 setActiveJobType(item);
                 router.push(`/search/${item}`)
               }}
-
             >
-              <Text>{item}</Text>
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
             </TouchableOpacity>
           )}
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small}}
+          horizontal
         />
       </View>
     </View>
