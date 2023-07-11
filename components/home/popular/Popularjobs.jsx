86b7d3252c062374e26c88963b84921cb0a449d2
Quyana Barrow <quyanabarrow@Quyanas-MBP.lan>
2023-07-11 15:13:38 -0400

    first API call to JSearch search endpoint

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -9,11 +9,36 @@ import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 
 const Popularjobs = () => {
   const router = useRouter();
+  const isLoading = false;
+  const error = false;
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Popularjobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary}/>
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList 
+            data={[1, 2, 3, 4, 5, 6, 7, 8]}
+            renderItem={({ item }) => (
+              <PopularJobCard
+                item={item}
+              />
+            )}
+            keyExtractor={item => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )} 
       </View>
     </View>
   )
